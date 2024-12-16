30efb9a04563d144a331c9c781add549e6a069f3
ren <53831277+Nicolassama@users.noreply.github.com>
2024-12-16 21:14:20 +0900

    Update signup.py

--- accounts/forms/signup.py
+++ accounts/forms/signup.py
@@ -1,16 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
-
 class SignUpForm(UserCreationForm):
-    usable_password = None
-
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
 
-class LoginFrom(AuthenticationForm):
-    class Meta:
-        model = User
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("このメールアドレスは既に登録されています。")
+        return email
+
+class LoginForm(AuthenticationForm):
+    pass
